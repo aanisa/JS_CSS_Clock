ad51232bc9e7771d2db8a93bdd81e98a71bbe6c6
Anisa <anisa.ali7@gmail.com>
2017-07-19 16:08:00 -0500

    seconds hand

--- client.js
+++ client.js
@@ -1,1 +1,9 @@
-console.log('JS LOad');
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const secondsDegrees = ((seconds/60) * 360);
+
+  console.log(secondsDegrees);
+}
+
+// setInterval(setDate, 1000);
